865ab4ea55d90534c1b87b591b382392c33d1595
Eduardo Neto <eduardo.js_jp@hotmail.com>
2021-10-11 17:00:33 -0300

    exec001

---
+++ src/exec001.ts
@@ -0,0 +1,84 @@
+import  readdirSync  from "readline-sync";
+
+// var
+
+
+/*let a,b: number;
+
+a = 10;
+b = 20;
+
+console.log(b);
+b = 5;
+console.log(`${a},${b}`)
+*/
+
+//var 
+
+/*let a, b, c;
+
+a = 30;
+b = 20;
+c = a + b;
+
+console.log(c);
+
+b = 10;
+
+console.log(b , c);
+
+c = a + b;
+
+console.log(a , b, c);
+*/
+
+//var
+ 
+/*let a, b, c;
+
+a = 10;
+b = 20;
+c = a;
+b = c;
+a = b;
+
+console.log(a, b, c);
+*/
+
+//var 
+
+/*let a, b: number ;
+
+a = 10;
+b = a + 1;
+a = b + 1;
+b = a + 1;
+console.log(a);
+a = b + 1;
+console.log(a, b);
+*/
+
+//var 
+
+/*let a, b, c: number;
+
+a = 10;
+b = 5;
+c = a + b;
+b = 20;
+a = 10;
+console.log(a, b, c);
+*/
+
+//var
+ 
+/*let x, y, z: number;
+
+x = 1;
+y = 2;
+z = y - x;
+console.log(z);
+x = 5;
+y = x + z;
+console.log(x, y, z);
+*/
